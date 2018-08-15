e9f87c4f98211bb05ed4503bcd5fade3e65e268d
Ryan Graham <ryangrahamwpb@gmail.com>
2018-08-15 15:34:06 -0400

    Add layout and burger builder files.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/burgerBuilder/burgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout >
-
+         <BurgerBuilder />
         </Layout>
       </div>
     );
