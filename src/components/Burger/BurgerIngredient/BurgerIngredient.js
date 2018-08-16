46b0009842f9f5e5fe630ae45286920a15cbf111
Ryan Graham <ryangrahamwpb@gmail.com>
2018-08-16 10:50:17 -0400

    Add Switch statement & prop types.

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classes from './burgerIngredient.css'
+
+class BurgerIngredient extends Component {
+    render() {
+        switch (this.props.type) {
+            case ('bread-bottom'):
+                ingredient = <div className={classes.BreadBottom}></div>;
+                break;
+            case ('bread-top'):
+                ingredient = (
+                    <div className={classes.BreadTop}>
+                        <div className={classes.Seeds1}></div>
+                        <div className={classes.Seeds2}></div>
+                    </div>
+                );
+                break;
+            case ('salad'):
+                ingredient = <div className={classes.Salad}></div>;
+                break;
+            case ('cheese'):
+                ingredient = <div className={classes.Cheese}></div>;
+                break;
+            case ('bacon'):
+                ingredient = <div className={classes.Bacon}></div>;
+                break;
+            case ('meat'):
+                ingredient = <div className={classes.Meat}></div>;
+                break;
+            default:
+                ingredient = null;
+        }
+        return ingredient;
+    }
+
+};
+
+BurgerIngredient.PropTypes = {
+    type: PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;
