c28e67be9e198df71ddc33d852cb9dc918df0e8d
Ryan Graham <ryangrahamwpb@gmail.com>
2018-08-16 12:09:51 -0400

    Add ingredients to burger and finalize css..

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classes from './burgerIngredient.css'
+import classes from './BurgerIngredient.css'
 
 class BurgerIngredient extends Component {
     render() {
+        let ingredient = null;
         switch (this.props.type) {
             case ('bread-bottom'):
                 ingredient = <div className={classes.BreadBottom}></div>;
